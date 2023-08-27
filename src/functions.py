7bdcd482caa17b0c43163e91f877226a1e4b4319
I. Ceyhun Andaç <andac@kyos.com>
2023-08-27 02:49:24 +0200

    Two trial functions to check max pnl

--- src/functions.py
+++ src/functions.py
@@ -25,3 +25,64 @@ def load_csv():
         input_csv = pd.read_csv(read_file)
 
     return input_csv
+
+
+def greedy_algorithm(df):
+    # Add a column for the PnL contribution of each trade
+    df.loc[:, 'PnL_Contribution'] = df['Price'] * df['Quantity']
+
+    # Sort the DataFrame based on PnL contribution
+    df_sorted = df.sort_values(by='PnL_Contribution', ascending=False)
+
+    # Initialize variables
+    cumulative_position = 0
+    total_pnl = 0
+
+    # List to store selected trades
+    selected_trades = []
+
+    # Iterate through the sorted DataFrame
+    for index, row in df_sorted.iterrows():
+        # Check if adding the current trade breaches the position limit
+        if abs(cumulative_position + row['Quantity']) <= 100000:
+            cumulative_position += row['Quantity']
+            total_pnl += row['PnL_Contribution']
+            selected_trades.append(index)
+
+    return selected_trades, total_pnl
+
+
+def simulated_annealing(df, initial_temp, cooling_rate, num_iterations):
+    # Initial solution (randomly select trades)
+    current_solution = np.random.choice([0, 1], size=len(df))
+    current_pnl = np.sum(current_solution * df['Price'] * df['Quantity'])
+
+    current_temp = initial_temp
+
+    for iteration in range(num_iterations):
+        # Perturb the solution (flip a random trade)
+        new_solution = current_solution.copy()
+        random_index = np.random.randint(0, len(df))
+        new_solution[random_index] = 1 - new_solution[random_index]
+
+        # Calculate new PnL
+        new_pnl = np.sum(new_solution * df['Price'] * df['Quantity'])
+
+        # Change in PnL
+        delta_pnl = new_pnl - current_pnl
+
+        # Acceptance probability
+        if delta_pnl > 0:
+            acceptance_prob = 1
+        else:
+            acceptance_prob = np.exp(delta_pnl / current_temp)
+
+        # Accept new solution based on acceptance probability
+        if np.random.rand() < acceptance_prob:
+            current_solution = new_solution
+            current_pnl = new_pnl
+
+        # Reduce temperature
+        current_temp *= cooling_rate
+
+    return current_solution, current_pnl
--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ import seaborn as sns
 
 # Internal modules
 from src.utils import add_to_log
-from src.functions import load_csv
+from src.functions import load_csv, greedy_algorithm, simulated_annealing
 
 
 def main():
@@ -19,14 +19,58 @@ def main():
     """
     # Read the input file
     df_input = load_csv()
+    df = df_input.copy()
 
     # Clean and sort the data
-    print(df_input.isna().sum())  # No NaN values in the dataset
-    print(df_input.isnull().sum())  # No null values in the dataset
+    print(df.isna().sum())  # No NaN values in the dataset
+    print(df.isnull().sum())  # No null values in the dataset
 
-    # df.describe().to_csv(path_or_buf="./outputs/data_summary.csv", sep=",", columns=df.columns)
-
-    # Visualization
+    df.describe().to_csv(path_or_buf="./outputs/data_summary.csv", sep=",", columns=df.columns)
 
     add_to_log("The given data is read and cleaned.")
-    a = 5
+
+    # Analysis and calculation
+    # We assume the initial position of the "ideal trader(s)" is zero.
+
+    # Since we checked and saw that there are positions quite large, let's first filter the dataset
+    # by removing the positions exceeding 200000, since they always make the position larger than
+    # 100000 or less than -100000.
+    filtered_df = df[abs(df['Quantity']) <= 200000]
+
+    # Multiply the positions with -1 since actually sells, i.e. negative positions, cumulatively
+    # effect the PnL and we are going to optimize the PnL.
+    filtered_df.loc[:, 'Quantity'] = filtered_df['Quantity'] * -1
+
+    # Assume the "ideal strategy" means a strategy that makes the owner earns the most PnL
+    filt_df = filtered_df.copy(deep=True)
+    # Apply greedy algortihm
+    # selected_trades, max_pnl = greedy_algorithm(filt_df)
+    # print(f"Selected trades indices: {selected_trades}")
+    # print(f"Number of selected trades: {len(selected_trades)}")
+    # print(f"Maximum PnL: {max_pnl}")
+
+    # Parameters for simulated annealing
+    initial_temp = 1000
+    cooling_rate = 0.995
+    num_iterations = 10000
+
+    solution, max_pnl = simulated_annealing(filt_df, initial_temp, cooling_rate,
+                                            num_iterations)
+    selected_trade_indices = np.where(solution == 1)[0]
+    print(f"Number of selected trades indices: {len(selected_trade_indices)}")
+    print(f"Maximum PnL: {max_pnl}")
+
+    # # Visualization
+    # # Plot all trades
+    # sns.scatterplot(data=filt_df, x=filt_df.index, y='Quantity', label='All Trades')
+    #
+    # # Overlay the "ideally" traded ones with red circles
+    # ideal_trades = filt_df.loc[selected_trades]
+    # sns.scatterplot(data=ideal_trades, x=ideal_trades.index, y='Quantity', color='red',
+    #                 label='Ideal Trades', s=50)
+    #
+    # plt.xlabel('Trade Index')
+    # plt.ylabel('Quantity')
+    # plt.title('All Trades vs. Ideal Trades')
+    # plt.legend()
+    # plt.show()
